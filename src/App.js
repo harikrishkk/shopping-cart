66870b524cbebcb412ed92507bdf17250f6f71f9
Harikrishnan <harikrishkk@gmail.com>
2022-02-17 17:32:25 -0500

    commit 10: add movies api axios call with load more

--- src/App.js
+++ src/App.js
@@ -6,36 +6,13 @@ import Footer from './components/Footer';
 import LuckyDraw from './components/LuckyDraw';
 import FeedbackForm from './components/FeedbackForm';
 import { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
-const MOVIES = [
-  {
-    id: 1,
-    title: 'Inside Out',
-    posterURL:
-      'https://m.media-amazon.com/images/M/MV5BOTgxMDQwMDk0OF5BMl5BanBnXkFtZTgwNjU5OTg2NDE@._V1_SX300.jpg',
-    imdbId: 'tt2096673',
-    isNewRelease: true,
-  },
-  {
-    id: 2,
-    title: 'Coco',
-    posterURL:
-      'https://m.media-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_SX300.jpg',
-    imdbId: 'tt2380307',
-    isNewRelease: false,
-  },
-  {
-    id: 3,
-    title: 'Incredibles 2',
-    posterURL:
-      'https://m.media-amazon.com/images/M/MV5BMTEzNzY0OTg0NTdeQTJeQWpwZ15BbWU4MDU3OTg3MjUz._V1_SX300.jpg',
-    imdbId: 'tt3606756',
-    isNewRelease: true,
-  },
-];
 function App() {
   const [movies, setMovies] = useState([]);
+  const [pages, setPages] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
   const handleMovieSelect = (movie) => {
     setSelectedMovie(movie);
   };
@@ -43,15 +20,25 @@ function App() {
     setSelectedMovie(null);
   };
 
+  const loadMore = () => {
+    setPages((p) => p + 1);
+  };
+
   useEffect(() => {
-    const id = setTimeout(() => {
-      setMovies(MOVIES);
-    }, 2000);
-    return () => {
-      console.log('Clean up before next cycle');
-      clearTimeout(id);
-    };
-  }, []);
+    axios
+      .get('https://api.sampleapis.com/movies/animation')
+      .then((response) => {
+        console.log(response);
+        const data = response.data.slice(
+          pages === 1 ? 0 : pages * 10 + 1,
+          pages === 1 ? 10 : pages * 10 + 10
+        );
+        setMovies([...movies, ...data]);
+        if (pages >= Math.floor(response.data.length / 10)) {
+          setIsDisabled(true);
+        }
+      });
+  }, [pages]);
 
   return (
     <Fragment>
@@ -83,6 +70,15 @@ function App() {
                       />
                     );
                   })}
+              </div>
+              <div className="my-8 flex align-center justify-center">
+                <button
+                  disabled={isDisabled}
+                  onClick={loadMore}
+                  className="btn btn-sm"
+                >
+                  Load More
+                </button>
               </div>
             </div>
           </section>
