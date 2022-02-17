6cfabb219124e76c41b87e22f2c6b7002f696550
Harikrishnan <harikrishkk@gmail.com>
2022-02-16 23:37:50 -0500

    commit 2: add tailwind, daisy and a hello world component, update README steps

---
+++ README.md
@@ -0,0 +1,15 @@
+## Install tailwind with CRA
+
+- https://tailwindcss.com/docs/guides/create-react-app
+
+## Install daisy UI
+
+- https://daisyui.com/docs/install/
+
+## We saw:
+
+- what is react
+- Its need.
+- component base architecture, declarative, reuse
+- SPA
+- Snippets: Es7 snippet in VS Code, chrome react dev tools.
--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello React </h1>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="text-center hero-content">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello React</h1>
+          <p className="py-6">let's learn some react stuff!</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
