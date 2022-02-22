29a5efb26d62c090781fe9a73405450499025bd9
Harikrishnan KK <harikrishkk@gmail.com>
2022-02-22 18:22:31 -0500

    commit 21: add lazy loading

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import Navbar from '@shared/Navbar';
 import Footer from '@shared/Footer';
-import { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@pages/Home';
-import Users from '@pages/Users';
-import About from '@pages/About';
 import NoFound from '@shared/NoFound';
 import AuthWrapper from '@shared/AuthWrapper';
+
+const Users = React.lazy(() => import('@pages/Users'));
+const About = React.lazy(() => import('@pages/About'));
 
 function App() {
   return (
@@ -18,8 +19,22 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="users" element={<Users />} />
-            <Route path="about" element={<About />} />
+            <Route
+              path="users"
+              element={
+                <React.Suspense fallback={<h1>Loading Page..</h1>}>
+                  <Users />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="about"
+              element={
+                <React.Suspense fallback={<h1>Loading Page..</h1>}>
+                  <About />
+                </React.Suspense>
+              }
+            />
             <Route path="*" element={<NoFound />} />
           </Routes>
         </div>
